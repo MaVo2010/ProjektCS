543f93e5352d0723913a1d92289f6c5dc1b6e914
unknown <marvin.nestler@student.htw-berlin.de>
2021-10-23 21:30:14 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech/ProjektCS/ProjektCsApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.ProjektCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektCsApplication.class, args);
+	}
+
+}
